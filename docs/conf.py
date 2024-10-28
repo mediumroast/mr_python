fd40db8a34cfb3e13c4f4e8793628588ae4bca3b
mihay42 <mihay42@gmail.com>
2024-10-28 05:13:14 -0700

    Fix rtd theme name

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ release = '0.6.12.17'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx-rtd-theme"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -29,5 +29,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx-rtd-theme'
 html_static_path = ['_static']
